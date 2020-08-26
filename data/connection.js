6a08b8fc760bf9087faab2170936b0c7804f9aa8
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-26 11:18:35 -0400

    Tweak

--- data/connection.js
+++ data/connection.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const knex = require("knex");
 
 const knexfile = require("../knexfile.js");
-const environment = process.env.NODE_ENV || "development";
 
-module.exports = knex(knexfile[environment]);
+module.exports = knex(knexfile.development);
